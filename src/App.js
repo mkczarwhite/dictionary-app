418bfe9ae33ff2e3b28b7361541849f588c76cb4
mkczarwhite <79484521+mkczarwhite@users.noreply.github.com>
2021-06-15 08:13:20 -0700

    created dictionary form and basic submit features

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from "react";
 import './App.css';
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-    <header className="App-header">
+      <div className="container">
+ <header className="App-header">
       Search Away!
       </header>
+      <main>
+        <Dictionary/>
+      </main>
+      <footer className="text-center">Coded by Michelle White</footer>
+      </div>
+   
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,20 @@
+import React, {useState} from "react";
+import './Dictionary.css';
+
+export default function Dictionary(){
+    let [keyword, setKeyword]=useState("");
+    function handleKeywordChange(event){
+        event.preventDefault();
+        setKeyword(event.target.value);
+
+    }
+    function search(event){
+        event.preventDefault();
+        alert (`Searching for ${keyword}`);
+    }
+    return <div className="Dictionary">
+        <form onSubmit={search}>
+            <input type="search" autoFocus={true} onChange={handleKeywordChange}/>
+        </form>
+    </div>;
+}
