9912e45899bceac95d5bd2aa605f03e7a32f5c8c
mkczarwhite <79484521+mkczarwhite@users.noreply.github.com>
2021-06-15 18:22:08 -0700

    created new component to handle meanings

--- src/Results.js
+++ src/Results.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Meaning from './Meaning';
 
 export default function Results(props){
     if (props.results){
@@ -6,7 +7,11 @@ export default function Results(props){
         <h2>
             {props.results.word} </h2>
             {props.results.meanings.map(function(meanings, index){
-                return meanings.definitions[0].definition;
+                return (
+                    <div key={index}>
+                        <Meaning />
+                        </div>
+                )
             })}
         
     </div>)
