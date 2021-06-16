4a594a3c20a1fff86dfd21be5ff1effc504e24ce
mkczarwhite <79484521+mkczarwhite@users.noreply.github.com>
2021-06-15 18:42:17 -0700

    created mapping function to move through definitions and examples

--- src/Results.js
+++ src/Results.js
@@ -6,10 +6,10 @@ export default function Results(props){
         return (<div className="Results">
         <h2>
             {props.results.word} </h2>
-            {props.results.meanings.map(function(meanings, index){
+            {props.results.meanings.map(function(meaning, index){
                 return (
                     <div key={index}>
-                        <Meaning />
+                        <Meaning meaning={meaning} />
                         </div>
                 )
             })}
