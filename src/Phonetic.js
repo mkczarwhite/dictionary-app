c5a7f28ed52fba93e54f9790b5262ad5f515d51a
mkczarwhite <79484521+mkczarwhite@users.noreply.github.com>
2021-06-23 18:27:48 -0700

    tried to create spacing between phonetic and sound

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -4,11 +4,14 @@ import "./Phonetics.css";
 export default function Phonetic (props){
     return(
         <div className="Phonetic">
-            <a href={props.phonetic.audio} target="_blank" rel="noreferrer">Listen</a>
-            <br/>
-            <div className="phonetic-text">
+            <div className="row">
+        <a href={props.phonetic.audio} target="_blank" rel="noreferrer">Listen</a></div>
+            <div className="row">
+                <div className="phonetic-text">
             {props.phonetic.text}
             </div>
+            </div>
+            
         </div>
     )
 }
